2779ff4320504f5056832ebf93c07b599cfd73a4
GislaineJessica <GislainehJessica@gmail.com>
2019-11-21 16:19:57 -0300

    Rate Limit

--- modulo16/README.md
+++ modulo16/README.md
@@ -4,6 +4,12 @@
     yarn add express-brute 
     yarn add express-brute-redis
     yarn add helmet
+    yarn add express-rate-limit 
+    yarn add rate-limit-redis
+    yarn add redis
   ```
 - Fazer o uso dessas bibliotecas na rota do servidor
-- Helmet é usado como um middleware do express no app.js
+- Helmet é usado como um middleware do express no app.js: configura headers que ajudam na proteção da aplicação
+
+- Rate limit: ;;;
+
--- modulo16/src/app.js
+++ modulo16/src/app.js
@@ -1,7 +1,13 @@
 import 'dotenv/config'
+
 import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
+
+import redis from 'redis'
+import RateLimit from 'express-rate-limit'
+import RateLimitRedis from 'rate-limit-redis'
+
 import 'express-async-errors'
 import routes from './routes'
 import path from 'path'
@@ -32,6 +38,18 @@ class App {
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
     )
+    if (process.env.NODE_ENV !== 'development'){
+      this.server.use(new RateLimit({
+        store: new RateLimitRedis({
+          client: redis.createClient({
+            host: process.env.REDIS_HOST,
+            port: process.env.REDIS_POST,
+          })
+        }),
+        windowMs: 1000 * 60 * 15,
+        max: 100,
+      }))
+    }
   }
 
   routes() {
